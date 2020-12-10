3165c6cdbca04b18fad8850efb5bee5c0472eea3
Jhosefer Senna <jhosefer_nf@hotmail.com>
2020-12-10 17:09:00 -0300

    config pastas

--- index.php
+++ index.php
@@ -6,30 +6,53 @@ function home()
     include("paginas/index.php");
 }
 
-function about_us()
+function criarProduto()
 {
     echo 'This is about us page. About Us template in HTML.';
 }
 
-function contact_us()
+function listarProduto()
+{
+    die('Page not found. Please try some different url.');
+}
+
+function carregarProduto()
+{
+    die('Page not found. Please try some different url.');
+}
+
+function editarProduto()
+{
+    die('Page not found. Please try some different url.');
+}
+
+function deletarProduto()
+{
+    die('Page not found. Please try some different url.');
+}
+
+function criarCategoria()
 {
     echo 'This is contact us page. Contact Us template in HTML.';
 }
 
 function page404()
 {
-    die('Page not found. Please try some different url.');
+    echo 'This is contact us page. Contact Us template in HTML.';
 }
+
+
+
 
 //If url is http://localhost/route/home or user is at the maion page(http://localhost/route/)
 if ($request == 'home' or $request == '')
     home();
 //If url is http://localhost/route/about-us
-else if ($request == 'about-us')
-    about_us();
+else if ($request == 'criar-produto')
+    criarProduto();
 //If url is http://localhost/route/contact-us
-else if ($request == 'contact-us')
-    contact_us();
+else if ($request == 'criar-categoria')
+    criarCategoria();
 //If user entered something else
 else
     page404();
